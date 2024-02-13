ab2c526caff6eb5e4a6723769b8a4b4325a23952
Pranav2456 <pranavvinodan@gmail.com>
2024-02-13 23:22:13 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className = "grid grid-cols-3">
     <RevenueCard title = {"Amount Pending"} amount = {"92,312.20"} orderCount = {13} />
+    <RevenueCard title = {"Amount Paid"} amount = {"92,312.20"} orderCount = {13} />
     </div>
   )  
 }
